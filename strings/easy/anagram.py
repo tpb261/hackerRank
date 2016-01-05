776e88852ee94aedd947edcb69041fd91b039b55
tpb261 <tpb261@gmail.com>
2016-01-05 12:24:30 +0530

    anagram problem and valid string problem

--- strings/easy/anagram.py
+++ strings/easy/anagram.py
@@ -7,25 +7,13 @@ for t in range(T):
     if(l%2==1):
         print(-1)
         continue
-    first=Counter(s[:l//2])
-    second=Counter(s[l//2:])    
+    first=s[:l//2]
+    second=s[l//2:]
     chng=0
-    for c1 in range(65, 91):
-        if(first[c1]==second[c1]):
-            del(first[c1])
-            del(second[c1])
-            
-    for c1 in range(65, 91):
-        if(c1 in first.keys()):
-            for c2 in range(65, 91):
-                if (c2 in second.keys() and first[c1]==second[c2]):
-                    chng += 1
-                    del(second[c2])
-                    del(first[c1])
-                    break
-    if((len(first)>0 and len(second)==0)\
-       or (len(first)==0 and len(second)>0)):
-        print(-1)
-    else:
-        
-        print(chng)
+    for c1 in range(65, 92):
+        f1 = first.count(chr(c1))
+        s1 = second.count(chr(c1))
+        f1 += first.count(chr(c1+32))
+        s1 += second.count(chr(c1+32))
+        chng += abs(f1-s1)
+    print(chng//2)
---
+++ strings/easy/valid_strings.py
@@ -0,0 +1,37 @@
+S="ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd"#"jtqgugmcsxvdwidtcyqpogkdifapuloqykjfxruvfrshcehekoiwbpbrprahwvhliglyxynjotbaswnnnmxbkmcftvsdqajemeul"
+s=set(S)
+h={}
+let_count_1 = let_count_2 = imp = 0
+let_count_1_count=0
+let_count_2_count=0
+for c in s:
+    h[c] = S.count(c)    
+    if(h[c]==let_count_1):
+        let_count_1_count+=1
+    if(h[c]==let_count_2):
+        let_count_2_count+=1
+    if(0==let_count_1):
+        let_count_1=h[c]
+        let_count_1_count=1
+    elif(h[c]!=let_count_1 and 0==let_count_2):
+        let_count_2 = h[c]
+        let_count_2_count=1        
+    elif(h[c]!=let_count_1 and h[c]!=let_count_2):
+        imp=1
+        break
+if(let_count_1>let_count_2):
+    [let_count_1, let_count_2]=[let_count_2, let_count_1]
+    [let_count_1_count, let_count_2_count]=[let_count_2_count, let_count_1_count]
+    
+if(0==imp):
+    if(let_count_2-let_count_1<=1 and 1==let_count_2_count):
+        print("YES")
+    elif(0==let_count_1):
+        print("YES")
+    elif(1==let_count_1 and 1==let_count_1_count):
+        print("YES")
+    else:
+        print("NO")
+else:
+    print ("NO")
+    
